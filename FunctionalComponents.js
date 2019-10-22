6f1a80dda17728bbd3df4477c1a46055f3c37de4
brie-cervaza <46555343+brie-cervaza@users.noreply.github.com>
2019-10-21 20:35:05 -0700

    Adding a header to test out the <div>

--- FunctionalComponents.js
+++ FunctionalComponents.js
@@ -5,11 +5,15 @@ import ReactDOM from "react-dom"
 //will only return a single JSX element - get around it by wrapping it in a <div>!
 function MyApp(){
   return (
+      <div>
+        <h1> This should be Fun!</h1>
          <ul> 
               <li> 1 </li>
               <li> 2 </li>
               <li> 3 </li>
-         </ul>)
+         </ul>
+     </div> 
+     )
 }
 
 //the documentgetElementById("root") shows the program where to display the code you are writing
