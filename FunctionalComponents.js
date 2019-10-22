733cf84398076b5d4c9ca68787fe35bd7ea5c245
brie-cervaza <46555343+brie-cervaza@users.noreply.github.com>
2019-10-21 20:33:22 -0700

    Rendering JSX elements from a function

---
+++ FunctionalComponents.js
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+//always have some camel case
+//will only return a single JSX element - get around it by wrapping it in a <div>!
+function MyApp(){
+  return (
+         <ul> 
+              <li> 1 </li>
+              <li> 2 </li>
+              <li> 3 </li>
+         </ul>)
+}
+
+//the documentgetElementById("root") shows the program where to display the code you are writing
+//<MyApp/> is a self closing component - apparently there are other components you can have to accept child components
+ReactDOM.render( <MyApp/>, document.getElementById("root"))
