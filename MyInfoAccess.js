66714ae36352cba34efb253cceb9ed0091228900
Gabrielle Cervantes <cervang@wwu.edu>
2019-10-21 21:19:43 -0700

    Making sure the path matches the new path for MyInfo.js

--- MyInfoAccess.js
+++ MyInfoAccess.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
-import MyInfo from "./MyInfo"
+//changing the path so it describes the path correctly
+import MyInfo from "./Components/MyInfo"
 
 ReactDOM.render(<MyInfo />, document.getElementById("root"))
 //this will be able to access MyInfo from the different file destinations
