4ced26cbcacf369451f0e057843c01e23d4e6ca2
brie-cervaza <46555343+brie-cervaza@users.noreply.github.com>
2019-10-21 21:07:04 -0700

    Sharing functions
    Learning to share functions from different files

---
+++ MyInfoAccess.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import MyInfo from "./MyInfo"
+
+ReactDOM.render(<MyInfo />, document.getElementById("root"))
+//this will be able to access MyInfo from the different file destinations
